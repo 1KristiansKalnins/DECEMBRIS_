826e25022e8e02981b9f046de6a20c40d8793269
1KristiansKalnins <kristians.kalnins@grobinasskola.lv>
2023-12-18 14:36:53 +0200

    Init commit

---
+++ 2uzdevums.py
@@ -0,0 +1,6 @@
+skaitlis = 1
+while skaitlis < 33:
+     print(skaitlis)
+     skaitlis += 1
+else:
+     print("skaitlis 32 kvadrātā lielāks nekā 1000")
---
+++ 3uzdevums.py
@@ -0,0 +1,5 @@
+#šo uzdevumu pildīju ar skaitļiem 10-101
+if skaitlis in range(10,101):
+    if skaitlis%2==0:
+        print(skaitlis)
+
